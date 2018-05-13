7c9bd19026959f23f4e1056ace8334e6900cbecd
husjay <hushaojie725@163.com>
2018-05-14 00:04:11 +0800

    add

--- LeetCode/src/soultion/oon_3sum/_3sum.java
+++ LeetCode/src/soultion/oon_3sum/_3sum.java
@@ -21,6 +21,10 @@ public class _3sum {
 		int[] nums = {-1, 0, 1, 2, -1, -4};
 		System.out.println(threeSum(nums));
 		
+		int[] numss = {-1,2,1,-4};
+		int target = 1;
+		System.out.println(threeSumClosest(numss, target));
+		
 	}
 	
 	public static List<List<Integer>> threeSum(int[] nums) {
@@ -41,6 +45,33 @@ public class _3sum {
 					while(lo<hi && nums[lo]==nums[lo-1])lo++;
 					while(lo<hi && nums[hi]==nums[hi+1])hi--;
 				} else if(nums[lo]+nums[hi] > target){
+					hi--;
+				} else {
+					lo++;
+				}
+			}
+		}
+		
+		return result;
+    }
+	
+	public static int threeSumClosest(int[] nums, int target) {
+		int result = nums[0]+nums[1]+nums[2];
+		Arrays.sort(nums);
+		for(int i=0; i< nums.length - 2; i++) {
+			if(i > 0 && nums[i] == nums[i-1]){
+				continue;				
+			}
+			int lo = i + 1, hi = nums.length-1;
+			while(lo < hi) {
+				int sum = nums[lo] + nums[hi] + nums[i];
+				if(Math.abs(result - target) > Math.abs(sum - target)) {
+					result = sum;
+					if(result == target){
+						return result;
+					}
+				}
+				if(sum > target){
 					hi--;
 				} else {
 					lo++;
